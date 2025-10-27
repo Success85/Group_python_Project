d8c6c0e5352fe0d8c6bcc2681dec7ae49a1b6986
Natnael Ayele <n.eticha@alustudent.com>
2025-10-27 22:48:45 +0200

    add comments for create_message function

--- helper_functions.py
+++ helper_functions.py
@@ -30,11 +30,13 @@ def convert_to_binary(name, age):
     # return binary number of name and age
     return binary_value, binary_number
 
+# create a function that creates a dynamic message based on the inputs from the user
 def create_message(name, age, name_binary, age_binary):
+    # store the message
     message_lines = (
         f"Hello {name}, you are {age} years old\n"
         f"Name in binary: {name_binary}\n"
         f"Age in binary: {age_binary}\n"
     )
-
+    # return the message
     return message_lines
