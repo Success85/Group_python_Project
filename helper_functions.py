24af7fb1bff52a566367d9e341fb2dad77bdbb4e
Natnael Ayele <n.eticha@alustudent.com>
2025-10-27 22:37:07 +0200

    improve readabilty for the message by separating it with new line

--- helper_functions.py
+++ helper_functions.py
@@ -31,11 +31,10 @@ def convert_to_binary(name, age):
     return binary_value, binary_number
 
 def create_message(name, age, name_binary, age_binary):
-    message_lines = [
-        f"Hello {name}, you are {age} years old"
-        f"Name in binary: {name_binary}"
-        f"Age in binary: {age_binary}"
-    ]
+    message_lines = (
+        f"Hello {name}, you are {age} years old\n"
+        f"Name in binary: {name_binary}\n"
+        f"Age in binary: {age_binary}\n"
+    )
 
-    full_message = "\n".join(message_lines)
-    return full_message
+    return message_lines
